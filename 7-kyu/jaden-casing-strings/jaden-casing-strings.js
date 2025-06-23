c2f353456da17705c9fb8eb0fd8dafcf6f4b481b
Providence Souvenir Dushime <dusouvenir04@gmail.com>
2025-06-23 23:51:56 +0200

    Add solution - CodeHub

---
+++ 7-kyu/jaden-casing-strings/jaden-casing-strings.js
@@ -0,0 +1,9 @@
+Object.defineProperty(String.prototype, 'toJadenCase', {
+  value: function () {
+    return this
+      .split(" ")
+      .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(" ");
+  }
+});
+​
