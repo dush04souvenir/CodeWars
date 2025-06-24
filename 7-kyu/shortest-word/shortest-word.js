542dbd4d24649fc4faa83ba79caefa3a55752295
Providence Souvenir Dushime <dusouvenir04@gmail.com>
2025-06-24 23:44:47 +0200

    Update solution - CodeHub

--- 7-kyu/shortest-word/shortest-word.js
+++ 7-kyu/shortest-word/shortest-word.js
@@ -1,5 +1,6 @@
 function findShort(s){
-  const str = s.split(" ");
-  const length = str.map(word => word.length)
+  const str = s.split(' ');
+  const length = str.map(word => word.length);
   return Math.min(...length);
+  
 }
