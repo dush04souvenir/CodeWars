9105f50d06b328fed3d1098e5d170550ad8f61a9
Providence Souvenir Dushime <dusouvenir04@gmail.com>
2025-06-30 22:25:54 +0200

    Update beginner-series-number-3-sum-of-numbers.js

--- 7-kyu/beginner-series-number-3-sum-of-numbers/beginner-series-number-3-sum-of-numbers.js
+++ 7-kyu/beginner-series-number-3-sum-of-numbers/beginner-series-number-3-sum-of-numbers.js
@@ -8,3 +8,9 @@ function getSum(a, b)
   }
    return sum;
 }
+//best practice solution 
+function getSum(a, b) {
+  const min = Math.min(a, b);
+  const max = Math.max(a, b);
+  return ((max - min + 1) * (min + max)) / 2;
+}
