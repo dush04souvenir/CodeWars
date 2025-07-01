6731785797cc18d600497a8e0be211b16ecec68e
Providence Souvenir Dushime <dusouvenir04@gmail.com>
2025-07-01 22:12:09 +0200

    Add solution - CodeHub

---
+++ 8-kyu/a-needle-in-the-haystack/a-needle-in-the-haystack.js
@@ -0,0 +1,9 @@
+function findNeedle(haystack) {
+  // your code here
+  for(let i =0; i< haystack.length; i++){
+    const index = haystack.indexOf("needle");
+    return `found the needle at position ${index}`;
+​
+    }
+  }
+​
