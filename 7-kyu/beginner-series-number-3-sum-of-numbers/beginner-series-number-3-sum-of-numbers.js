20aaeb2a200d1ad2406155b13e71dd13ffc60440
Providence Souvenir Dushime <dusouvenir04@gmail.com>
2025-07-07 22:42:28 +0200

    Update solution - CodeHub

--- 7-kyu/beginner-series-number-3-sum-of-numbers/beginner-series-number-3-sum-of-numbers.js
+++ 7-kyu/beginner-series-number-3-sum-of-numbers/beginner-series-number-3-sum-of-numbers.js
@@ -1,16 +1,17 @@
-function getSum(a, b)
-{
-  let sum = 0;
-  const start = Math.min(a,b);
-  const end = Math.max(a,b);
-  for(let i =start; i <=end; i++){
-    sum +=i;
+function getSum(a, b){
+   const max = Math.max(a,b);
+   const min = Math.min(a,b);
+​
+  if( a === b){
+      return a;
+      
   }
-   return sum;
+  else{
+    let sum = 0;
+    for(let i = min; i <= max; i ++){
+      sum += i;
+    }
+    return sum;
+  }
 }
-//best practice solution 
-function getSum(a, b) {
-  const min = Math.min(a, b);
-  const max = Math.max(a, b);
-  return ((max - min + 1) * (min + max)) / 2;
-}
+console.log(getSum(-1,5));
