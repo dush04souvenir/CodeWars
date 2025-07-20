14d6c05a45b2381b3ea7eeb00ba16a2061fc7f4c
Providence Souvenir Dushime <dusouvenir04@gmail.com>
2025-07-20 17:40:44 +0200

    Add solution - CodeHub

---
+++ 6-kyu/counting-duplicates/counting-duplicates.js
@@ -0,0 +1,23 @@
+function duplicateCount(text){
+    let result = 0;
+    let numberOfDuplicate = 0;
+    let texta = text.toLowerCase();
+    let uniqueText = new Set(texta);
+    let newArr = [...uniqueText];
+    for(let i = 0; i< newArr.length; i++ ){
+      let index = newArr[i];
+      for(let j = 0; j < texta.length; j++){
+        if(texta[j] == index){
+           numberOfDuplicate++;
+        }
+      }
+      if(numberOfDuplicate > 1){
+        
+        result++;
+      }
+      numberOfDuplicate = 0;
+       
+    }
+     return result;
+  }
+​
