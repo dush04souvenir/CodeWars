061fe32dc73c84bb727d9b0bed80f97ae278758b
Providence Souvenir Dushime <dusouvenir04@gmail.com>
2025-07-22 13:03:55 +0200

    Add solution - CodeHub

---
+++ 6-kyu/find-the-odd-int/find-the-odd-int.js
@@ -0,0 +1,23 @@
+function findOdd(A) {
+  //happy coding!
+  let uniqueA = new Set(A);
+  let uniqueArray = [...uniqueA]
+  for(let i = 0; i < uniqueArray.length; i++){
+      let element = uniqueArray[i];
+      let numberOfRepeats = 0;
+      for(b of A){
+        
+          let current = b;
+          if(element == current){
+              numberOfRepeats++;
+              
+          }
+          
+        
+      }
+    if(numberOfRepeats % 2 !== 0){
+                  return element;
+              }
+     
+  }
+}
