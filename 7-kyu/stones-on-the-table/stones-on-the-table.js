9aecd3fb7f218b085f94c94a966669152c54cb20
Providence Souvenir Dushime <dusouvenir04@gmail.com>
2025-08-11 22:59:23 +0200

    Update solution - CodeHub

--- 7-kyu/stones-on-the-table/stones-on-the-table.js
+++ 7-kyu/stones-on-the-table/stones-on-the-table.js
@@ -1,6 +1,9 @@
+ 
 function solve(stones) {
+   let colors = 0;
    let red = stones.split("").filter(char => char === "R").length;
    let green = stones.split("").filter(char => char === "G").length;
    let blue = stones.split("").filter(char => char === "B").length;
+   colors = red % 2 + green % 2 + blue % 2;
    
 }
