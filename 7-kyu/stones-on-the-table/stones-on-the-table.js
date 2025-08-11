25352cf6321da35a9b6a518cdf931e87d39360f4
Providence Souvenir Dushime <dusouvenir04@gmail.com>
2025-08-11 22:57:26 +0200

    Add solution - CodeHub

---
+++ 7-kyu/stones-on-the-table/stones-on-the-table.js
@@ -0,0 +1,6 @@
+function solve(stones) {
+   let red = stones.split("").filter(char => char === "R").length;
+   let green = stones.split("").filter(char => char === "G").length;
+   let blue = stones.split("").filter(char => char === "B").length;
+   
+}
